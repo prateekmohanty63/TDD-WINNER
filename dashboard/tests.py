f4437cbc84f9b1d751b3773928e9b839c642beac
Prateek <prateekmohanty63@gmail.com>
2023-02-06 23:22:45 +0530

    Feat(TDD) Test_user_password

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -10,14 +10,23 @@ class UserTestCast(TestCase):
 
     def setUp(self):
         user_a=User(username='testAdmin',email='testAdmin@gmail.com')
+
+        user_a_pw="testAdminPassword"
+        self.user_a_pw=user_a_pw
         user_a.is_staff=True
         user_a.is_superuser=True
-        user_a.set_password('root123')
+        user_a.set_password(user_a_pw)
         user_a.save()
-        
+        self.user_a=user_a
+
     def test_user_exists(self):
         user_count=User.objects.all().count()
-        self.assertEqual(user_count,1)
-        self.assertNotEqual(user_count,0)
+        self.assertEqual(user_count,1)   # checking equal
+        self.assertNotEqual(user_count,0)   # checking not equal
+    
+    def test_user_password(self):
+        self.assertTrue(self.user_a.check_password(self.user_a_pw))
+
+
     
    
