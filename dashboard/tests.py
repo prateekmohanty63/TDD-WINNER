8c5d9dc9df7d85699ac701cd78b1be51edabfc4b
Prateek <prateekmohanty63@gmail.com>
2023-02-06 23:47:39 +0530

    Feat(TDD) Test_user_login || glitch

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -26,6 +27,33 @@ class UserTestCast(TestCase):
     
     def test_user_password(self):
         self.assertTrue(self.user_a.check_password(self.user_a_pw))
+
+    def test_login_url(self):
+        # login_url="/login/"
+        # self.assertEqual(settings.LOGIN_URL,login_url)
+
+        login_url=settings.LOGIN_URL
+
+        # python requests - manage.py runserver
+        # self.client.get , self.client.post
+
+        # response=self.client.post(url,{},follow=True)
+
+        data={"username":"testAdmin","password":"testAdminPassword"}
+        response=self.client.post(login_url,data,
+        follow=True)
+        # print(dir(response))
+        print(response.request)
+
+        status_code=response.status_code
+        redirect_path=response.request.get("PATH_INFO")
+        # self.assertEqual(redirect_path,settings.LOGIN_REDIRECT_URL)
+        # self.assertEqual(status_code,200)
+
+
+
+
+
 
 
     
