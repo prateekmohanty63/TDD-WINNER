877410e6842068ca22e7c92d9675f65c263d428c
Prateek <prateekmohanty63@gmail.com>
2023-02-06 23:02:40 +0530

    Feat(TDD) setup for testing done

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+# TDD
+
+User=get_user_model()
+
+class UserTestCast(TestCase):
+
+    def setUp(self):
+        user_a=User(username='testAdmin',email='testAdmin@gmail.com')
+        user_a.is_staff=True
+        user_a.is_superuser=True
+        user_a.set_password('root123')
+        user_a.save()
+        print(user_a.id)
+    
+    def test_user_exists(self):
+        user_count=User.objects.all().count()
+        print(user_count)
+        self.assertEqual(user_count,1)
+        self.assertNotEqual(user_count,0)
